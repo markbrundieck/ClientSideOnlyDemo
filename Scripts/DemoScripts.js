57871004dd6e903e6b61fcd6271ec033ee7ee909
MarkBrundieck <markbrundieck@users.noreply.github.com>
2016-11-10 18:31:41 -0600

    committing production files

---
+++ Scripts/DemoScripts.js
@@ -0,0 +1,10 @@
+﻿/* hide show of sections*/
+function ShowDefault() {
+    $("#TempConvertDisplay").css("display","none");
+    $("#DefaultDisplay").css("display", "block");
+}
+
+function ShowTempConversion() {
+    $("#TempConvertDisplay").show();
+    $("#DefaultDisplay").hide();
+}
---
+++ Scripts/utilityScripts.js
@@ -0,0 +1,32 @@
+﻿/* temperature conversion */
+function CelsiusToFahr(c) {
+    if (isNaN(c)) {
+        $("#validationDisplay").text('Only enter a number.');
+        return;
+    }
+    else {
+        $("#validationDisplay").text('');
+    }
+    
+    if (c < (-273.15)) {
+        $("#validationDisplay").text('Value is below absolute zero');
+        return;
+    } else {
+        return 180 / 100 * c + 32;
+    }
+}
+function FahrToCelsius(f) {
+    if (isNaN(f)) {
+        $("#validationDisplay").text('Only enter a number.');
+        return;
+    }
+    else {
+        $("#validationDisplay").text('');
+    }
+    if (f < (-459.67)) {
+        $("#validationDisplay").text('Value is below absolute zero');
+        return;
+    } else {
+        return 100 / 180 * (f - 32);
+    }
+}
